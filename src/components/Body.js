75cc99f46d21e96d7382ea2ea163027476c0fa89
Bunty9 <cbipin2000@gmail.com>
2021-09-17 21:48:04 +0530

    final changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,82 @@
-import React from 'react'
+import React from "react";
 import "./Body.css";
+import Header from "./Header";
+import { useStateValue } from "../StateProvider";
+import SongRow from "./SongRow";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
-function Body() {
-    return (
-        <div className="body">
-            <h1>Body</h1>
-        </div>
-    )
+function Body({ spotify }) {
+	const [{ discover_weekly }, dispatch] = useStateValue();
+
+	const playPlaylist = (id) => {
+		spotify
+			.play({
+				context_uri: `spotify:playlist:4xxIBgTO71lpeGEwghpoqz`,
+			})
+			.then((res) => {
+				spotify.getMyCurrentPlayingTrack().then((r) => {
+					dispatch({
+						type: "SET_ITEM",
+						item: r.item,
+					});
+					dispatch({
+						type: "SET_PLAYING",
+						playing: true,
+					});
+				});
+			});
+	};
+
+	const playSong = (id) => {
+		spotify
+			.play({
+				uris: [`spotify:track:${id}`],
+			})
+			.then((res) => {
+				spotify.getMyCurrentPlayingTrack().then((r) => {
+					dispatch({
+						type: "SET_ITEM",
+						item: r.item,
+					});
+					dispatch({
+						type: "SET_PLAYING",
+						playing: true,
+					});
+				});
+			});
+	};
+	// console.log(discover_weekly);
+
+	return (
+		<div className="body">
+			<Header spotify={spotify} />
+			<div className="body__info">
+				<img src={discover_weekly?.images[0].url} alt="" />
+				<div className="body__infoText">
+					<strong>Discover Weekly</strong>
+					<h2>{discover_weekly?.name}</h2>
+					<p>{discover_weekly?.description}</p>
+				</div>
+			</div>
+
+			<div className="body__songs">
+				<div className="body__icons">
+					<PlayCircleFilledIcon
+						className="body__shuffle"
+						onClick={playPlaylist}
+					/>
+					<FavoriteIcon fontSize="large" />
+					<MoreHorizIcon />
+				</div>
+
+				{discover_weekly?.tracks.items.map((item, i) => (
+					<SongRow key={i} playSong={playSong} track={item.track} />
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default Body
+export default Body;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React from "react";
 import "./Sidebar.css";
-
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import AddBoxIcon from "@material-ui/icons/AddBox";
 import { useStateValue } from "../StateProvider";
 
 function Sidebar() {
-    const [{ playlists }, dispatch] = useStateValue();
-    console.log(playlists);
+	const [{ playlists }] = useStateValue();
+	// console.log(playlists);
 
-    return (
-        <div className = "sidebar">
-            <img
-            className="sidebar__logo"
-            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-            alt=""
-            />
-            
-        </div>
-    )
+	return (
+		<div className="sidebar">
+			<img
+				className="sidebar__logo"
+				src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+				alt=""
+			/>
+			<SidebarOption Icon={HomeIcon} option="Home" />
+			<SidebarOption Icon={SearchIcon} option="Search" />
+			<SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+			<br />
+			<SidebarOption Icon={AddBoxIcon} option="New Playlist" />
+			<SidebarOption Icon={FavoriteIcon} option="Your Likes" />
+			<br />
+			<hr />
+			{playlists?.items?.map((playlist, i) => (
+				<SidebarOption key={i} option={playlist.name} />
+			))}
+		</div>
+	);
 }
 
-export default Sidebar
+export default Sidebar;
